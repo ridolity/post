a02d0311857d9963b72ac71cfeba2dc6a0436dfb
ridolity <153475749+ridolity@users.noreply.github.com>
2024-06-26 21:39:37 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -137,21 +137,12 @@ if _G.Switch_Hub_Series_R then
 	if game:GetService("Players").LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
 		repeat task.wait()
 			if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main").ChooseTeam.Visible == true then
-				if getgenv().Config.Team == "Marines" then
-					for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container["Marines"].Frame.TextButton.Activated)) do
-						for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
-							b:Fire() 
-						end
-						v.Function()
-					end 
-				else
-					for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container["Pirates"].Frame.TextButton.Activated)) do
-						for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
-							b:Fire() 
-						end
-						v.Function()
-					end 
-				end
+				for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container["Pirates"].Frame.TextButton.Activated)) do
+					for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
+						b:Fire() 
+					end
+					v.Function()
+				end 
 			end
 		until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
 	end
